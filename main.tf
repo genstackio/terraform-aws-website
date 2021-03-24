8901f35f9c9865c940b719116f1e0ffb213e14a5
Olivier Hoareau <oha@greenberets.io>
2021-03-24 19:17:09 +0100

    fix: conflict between cache_policy and forward_values

--- main.tf
+++ main.tf
@@ -58,7 +58,7 @@ resource "aws_cloudfront_distribution" "website" {
     target_origin_id = "website-${var.name}-s3"
 
     dynamic "forwarded_values" {
-      for_each = var.cache_policy_id ? {} : {x: true}
+      for_each = (null != var.cache_policy_id) ? {} : {x: true}
       content {
         query_string = var.forward_query_string
         cookies {
