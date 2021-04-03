cef7493d026684b6d080aabc6f9ef72f234856cd
Olivier Hoareau <oha@greenberets.io>
2021-04-03 20:29:51 +0200

    fix: fix

--- modules/lambda-proxy/main.tf
+++ modules/lambda-proxy/main.tf
@@ -80,142 +80,141 @@ locals {
 
 module "log-group-us-east-2" {
   source  = "genstackio/lambda/aws//modules/log-group"
-  enabled = local.lg_us-east-2_enabled
   version = "0.1.5"
+  enabled = local.lg_us-east-2_enabled
   name    = var.name
   providers = { aws = aws.us-east-2 }
 }
 module "log-group-us-west-1" {
   source  = "genstackio/lambda/aws//modules/log-group"
-  enabled = local.lg_us-west-1_enabled
   version = "0.1.5"
+  enabled = local.lg_us-west-1_enabled
   name    = var.name
   providers = { aws = aws.us-west-1 }
 }
 module "log-group-us-west-2" {
   source  = "genstackio/lambda/aws//modules/log-group"
-  enabled = local.lg_us-west-2_enabled
   version = "0.1.5"
+  enabled = local.lg_us-west-2_enabled
   name    = var.name
   providers = { aws = aws.us-west-2 }
 }
 module "log-group-af-south-1" {
   source  = "genstackio/lambda/aws//modules/log-group"
-  enabled = local.lg_af-south-1_enabled
   version = "0.1.5"
+  enabled = local.lg_af-south-1_enabled
   name    = var.name
   providers = { aws = aws.af-south-1 }
 }
 module "log-group-ap-east-1" {
   source  = "genstackio/lambda/aws//modules/log-group"
-  enabled = local.lg_ap-east-1_enabled
   version = "0.1.5"
+  enabled = local.lg_ap-east-1_enabled
   name    = var.name
   providers = { aws = aws.ap-east-1 }
 }
 module "log-group-ap-south-1" {
   source  = "genstackio/lambda/aws//modules/log-group"
-  enabled = local.lg_ap-south-1_enabled
   version = "0.1.5"
+  enabled = local.lg_ap-south-1_enabled
   name    = var.name
   providers = { aws = aws.ap-south-1 }
 }
 module "log-group-ap-northeast-1" {
   source  = "genstackio/lambda/aws//modules/log-group"
-  enabled = local.lg_ap-northeast-1_enabled
   version = "0.1.5"
+  enabled = local.lg_ap-northeast-1_enabled
   name    = var.name
   providers = { aws = aws.ap-northeast-1 }
 }
 module "log-group-ap-northeast-2" {
   source  = "genstackio/lambda/aws//modules/log-group"
-  enabled = local.lg_ap-northeast-2_enabled
   version = "0.1.5"
+  enabled = local.lg_ap-northeast-2_enabled
   name    = var.name
   providers = { aws = aws.ap-northeast-2 }
 }
 module "log-group-ap-northeast-3" {
   source  = "genstackio/lambda/aws//modules/log-group"
-  enabled = local.lg_ap-northeast-3_enabled
   version = "0.1.5"
+  enabled = local.lg_ap-northeast-3_enabled
   name    = var.name
   providers = { aws = aws.ap-northeast-3 }
 }
 module "log-group-ap-southeast-1" {
   source  = "genstackio/lambda/aws//modules/log-group"
-  enabled = local.lg_ap-southeast-1_enabled
   version = "0.1.5"
+  enabled = local.lg_ap-southeast-1_enabled
   name    = var.name
   providers = { aws = aws.ap-southeast-1 }
 }
 module "log-group-ap-southeast-2" {
   source  = "genstackio/lambda/aws//modules/log-group"
-  enabled = local.lg_ap-southeast-2_enabled
   version = "0.1.5"
+  enabled = local.lg_ap-southeast-2_enabled
   name    = var.name
   providers = { aws = aws.ap-southeast-2 }
 }
 module "log-group-ca-central-1" {
   source  = "genstackio/lambda/aws//modules/log-group"
-  enabled = local.lg_ca-central-1_enabled
   version = "0.1.5"
+  enabled = local.lg_ca-central-1_enabled
   name    = var.name
   providers = { aws = aws.ca-central-1 }
 }
 module "log-group-eu-central-1" {
   source  = "genstackio/lambda/aws//modules/log-group"
-  enabled = local.lg_eu-central-1_enabled
   version = "0.1.5"
+  enabled = local.lg_eu-central-1_enabled
   name    = var.name
   providers = { aws = aws.eu-central-1 }
 }
 module "log-group-eu-west-1" {
   source  = "genstackio/lambda/aws//modules/log-group"
-  enabled = local.lg_eu-west-1_enabled
   version = "0.1.5"
+  enabled = local.lg_eu-west-1_enabled
   name    = var.name
   providers = { aws = aws.eu-west-1 }
 }
 module "log-group-eu-west-2" {
   source  = "genstackio/lambda/aws//modules/log-group"
-  enabled = local.lg_eu-west-2_enabled
   version = "0.1.5"
+  enabled = local.lg_eu-west-2_enabled
   name    = var.name
   providers = { aws = aws.eu-west-2 }
 }
 module "log-group-eu-west-3" {
   source  = "genstackio/lambda/aws//modules/log-group"
-  enabled = local.lg_eu-west-3_enabled
   version = "0.1.5"
+  enabled = local.lg_eu-west-3_enabled
   name    = var.name
   providers = { aws = aws.eu-west-3 }
 }
 module "log-group-eu-south-1" {
   source  = "genstackio/lambda/aws//modules/log-group"
-  enabled = local.lg_eu-south-1_enabled
   version = "0.1.5"
+  enabled = local.lg_eu-south-1_enabled
   name    = var.name
   providers = { aws = aws.eu-south-1 }
 }
 module "log-group-eu-north-1" {
   source  = "genstackio/lambda/aws//modules/log-group"
-  enabled = local.lg_eu-north-1_enabled
   version = "0.1.5"
+  enabled = local.lg_eu-north-1_enabled
   name    = var.name
   providers = { aws = aws.eu-north-1 }
 }
 module "log-group-me-south-1" {
   source  = "genstackio/lambda/aws//modules/log-group"
-  enabled = local.lg_me-south-1_enabled
   version = "0.1.5"
+  enabled = local.lg_me-south-1_enabled
   name    = var.name
   providers = { aws = aws.me-south-1 }
 }
 module "log-group-sa-east-1" {
   source  = "genstackio/lambda/aws//modules/log-group"
-  enabled = local.lg_sa-east-1_enabled
   version = "0.1.5"
+  enabled = local.lg_sa-east-1_enabled
   name    = var.name
   providers = { aws = aws.sa-east-1 }
 }
-
